bb6a258334133dec67444986ffd3584ae41c14d6
Dan George <dangeorge35@gmail.com>
2023-11-11 06:14:50 +0100

    updated

--- cadence_server/src/services/index.ts
+++ cadence_server/src/services/index.ts
@@ -1,11 +1,5 @@
-import fs from 'fs'
 import investorsEndpoint from './investors/investors.endpoint'
 import workersEndpoint from './workers/workers.endpoint'
-
-const dir = '../../public'
-if (!fs.existsSync(`.${dir}`)) {
-  fs.mkdirSync(`.${dir}`)
-}
 
 export default [
   ...investorsEndpoint,
