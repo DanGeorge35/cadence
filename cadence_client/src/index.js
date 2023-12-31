defb56c2ab472201dc9b4f7018c071c2984a2d35
Dan George <dangeorge35@gmail.com>
2023-12-31 15:35:17 +0100

    updated

--- cadence_client/src/index.js
+++ cadence_client/src/index.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import "animate.css";
 import "./dashboard.scss";
-
+//yes
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
