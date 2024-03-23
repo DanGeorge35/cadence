8892b3c8f685a1cd0fa28a0dd939bc8b42575ec7
Dan George <dangeorge35@gmail.com>
2024-03-23 08:19:59 +0100

    Updated

--- cadence_client/src/admin/component/DataDetails.jsx
+++ cadence_client/src/admin/component/DataDetails.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import $ from "jquery"; // Import jQuery
+import "datatables.net-autofill-bs5";
+import "datatables.net-buttons-bs5";
+import "datatables.net-buttons/js/buttons.colVis.mjs";
+import "datatables.net-buttons/js/buttons.html5.mjs";
+import "datatables.net-buttons/js/buttons.print.mjs";
+import "datatables.net-scroller-bs5";
+
 // import * as Icons from "@mui/icons-material";
 
 const dataStructure = (data) => {
@@ -7,22 +15,35 @@ const dataStructure = (data) => {
 
   console.log(data);
   // console.log(data);
+  record.tabs = [];
 
-  record.tabs = Object.keys(data).map((key) => {
-    return key + " Data";
+  record.TBheaders = {};
+  record.TBdata = {};
+  Object.keys(data).forEach((key) => {
+    record.tabs.push(key);
+    const dataContent = data[key];
+
+    if (dataContent.length > 0) {
+      console.log(dataContent[0]);
+      record.TBheaders[key] = Object.keys(dataContent[0]);
+      record.TBdata[key] = dataContent;
+    } else {
+      record.TBheaders[key] = [];
+      record.TBdata[key] = [];
+    }
   });
-  console.log(record.tabs);
-
-  record.TBheaders = Object.keys(data[0] || {});
 
   return record;
 };
 
 const DataDetails = ({ data }) => {
   const dTrans = dataStructure(data);
-  const tableHeaders = dTrans.TBheaders;
-  const tableData = data.ROIs;
-  console.log(tableHeaders, tableData);
+  const Tab = dTrans.tabs;
+  const TBheaders = dTrans.TBheaders;
+  const TBdata = dTrans.TBdata;
+  console.log(Tab);
+  console.log(TBheaders);
+  console.log(TBdata);
 
   if (data === null) {
     return (
@@ -33,6 +54,13 @@ const DataDetails = ({ data }) => {
     );
   }
 
+  useEffect(() => {
+    $(".thisdatatable").DataTable({
+      buttons: ["copy", "csv", "excel", "pdf", "print"],
+      dom: "Bfrtip", // Specify the location of the DataTable Buttons
+    });
+  }, []);
+
   return (
     <div className="p-3 border">
       <div
@@ -40,78 +68,58 @@ const DataDetails = ({ data }) => {
         style={{ borderRadius: "10px" }}
       >
         <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link tab active"
-              id="pills-home-tab"
-              data-bs-toggle="pill"
-              data-bs-target="#pills-home"
-              type="button"
-              role="tab"
-              aria-controls="pills-home"
-              aria-selected="true"
-            >
-              Home
-            </button>
-          </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link"
-              id="pills-profile-tab"
-              data-bs-toggle="pill"
-              data-bs-target="#pills-profile"
-              type="button"
-              role="tab"
-              aria-controls="pills-profile"
-              aria-selected="false"
-            >
-              Profile
-            </button>
-          </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link"
-              id="pills-contact-tab"
-              data-bs-toggle="pill"
-              data-bs-target="#pills-contact"
-              type="button"
-              role="tab"
-              aria-controls="pills-contact"
-              aria-selected="false"
-            >
-              Contact
-            </button>
-          </li>
+          {Tab.map((item, index) => (
+            <li key={index} className="nav-item" role="presentation">
+              <button
+                className={`nav-link tab ${index === 0 ? "active" : ""}`}
+                id={`pills-${index}-tab`}
+                data-bs-toggle="pill"
+                data-bs-target={`#pills-${index}`}
+                type="button"
+                role="tab"
+                aria-controls={`pills-${index}`}
+                aria-selected={index === 0 ? "true" : "false"}
+              >
+                {item}
+              </button>
+            </li>
+          ))}
         </ul>
         <div
           className="tab-content "
           id="pills-tabContent"
           style={{ marginTop: "35px" }}
         >
-          <div
-            className="tab-pane fade show active"
-            id="pills-home"
-            role="tabpanel"
-            aria-labelledby="pills-home-tab"
-          >
-            ...
-          </div>
-          <div
-            className="tab-pane fade"
-            id="pills-profile"
-            role="tabpanel"
-            aria-labelledby="pills-profile-tab"
-          >
-            ...
-          </div>
-          <div
-            className="tab-pane fade"
-            id="pills-contact"
-            role="tabpanel"
-            aria-labelledby="pills-contact-tab"
-          >
-            ...
-          </div>
+          {Tab.map((item, index) => (
+            <div
+              key={index}
+              className={`tab-pane fade   ${index === 0 ? " show active" : ""}`}
+              id={`pills-${index}`}
+              role="tabpanel"
+              aria-labelledby={`pills-${index}-tab`}
+            >
+              <table className="display nowrap table  thisdatatable table-striped table-auto ">
+                <thead>
+                  <tr>
+                    {TBheaders[item] &&
+                      TBheaders[item].map((headerItem, idx) => (
+                        <th key={idx}>{headerItem}</th>
+                      ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {TBdata[item] &&
+                    TBdata[item].map((rowData, rowIndex) => (
+                      <tr key={rowIndex}>
+                        {TBheaders[item].map((headerItem, columnIndex) => (
+                          <td key={columnIndex}>{rowData[headerItem]}</td>
+                        ))}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          ))}
         </div>
       </div>
     </div>
