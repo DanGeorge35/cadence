bb08752844aa4ac40fc17b3bf18b04d62747858f
Dan George <dangeorge35@gmail.com>
2024-06-01 21:09:44 +0100

    updated

--- cadence_client/src/components/JoinUs.jsx
+++ cadence_client/src/components/JoinUs.jsx
@@ -12,20 +12,19 @@ class JoinUs extends Component {
               </div>
               <div className="col-lg-6 pt-5 pb-5">
                 <h1 className="pb-3 text-primary-color">
-                  Join the Cadence Family
+                  Join the Cadence Family and Keep Earning!
                 </h1>
                 <div
                   className="fontFam3"
                   style={{ fontSize: "20px", fontWeight: "400" }}
                 >
-                  &quot;Sharing the best experiences begins with you! 🌟
-                  We&apos;re excited to invite potential investors to join the
-                  Cadence family. Cadence isn&apos;t just a place to dine and
-                  unwind; it&apos;s a unique investment opportunity covering a
-                  restaurant, lounge, bar, and short-let homes, all promising
-                  lucrative returns. Be part of our journey to create a legacy
-                  of excellence in hospitality. Don&apos;t miss out—reach out
-                  today to explore Cadence investment opportunities
+                  Unlock unparalleled investment opportunities with Cadence. Our
+                  luxurious shortlet apartments generate consistent, high-yield
+                  revenues, ensuring you receive steady returns. Invest with us
+                  and enjoy peace of mind knowing your funds are working in a
+                  thriving, lucrative market. Don&apos;t miss out—become a part
+                  of the Cadence family today and start earning from our
+                  success!
                   <div className="py-4">
                     <a
                       href="/signup"
